95e660c7b64ab862228ac02ba513cdcccc7b2680
ROFIIHSAN <rofshanunlimited@gmail.com>
2026-07-30 15:21:55 +0700

    up cpp

---
+++ cpp/telur.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int telur1 = 37;
+	int telur2 = 45;
+	int total = telur1 + telur2;
+
+	cout << "Sye membeli telur pertama : " << telur1 << " butir" << endl;
+	cout << "Sye membeli telur kedua : " << telur2 << " butir" << endl;
+	cout << "---------------------------------" << endl;
+	cout << "Jumlah telur seluruhnya : " << total << " butir" << endl;
+	
+}
